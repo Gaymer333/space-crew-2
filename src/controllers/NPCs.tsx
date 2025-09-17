47f72f6e08a72f53a633cc90f4d66d7e85304005
Peter Andresen <pa@onomondo.com>
2025-09-18 00:23:30 +0900

    Made a successful Relations ship update

--- src/controllers/NPCs.tsx
+++ src/controllers/NPCs.tsx
@@ -1,11 +1,12 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import NPC from "../types/NPCs";
 import startingNPCs from "../DemoDataDeleteMe/StartingNPCs";
 
 interface NPCsContextType {
   NPCs: NPC[];
   setNPCs: (npcs: NPC[]) => void;
-  updateNPCNeed: (npc: NPC, needId: string, amount: number) => void;
+  updateNPCNeed: (npcId: string, needId: string, amount: number) => void;
+  updateNPCRelationship: (npcId: string, otherNpcId: string, amount: number) => void;
 }
 
 const NPCsContext = createContext<NPCsContextType | undefined>(undefined);
@@ -33,18 +34,43 @@ function setupNPCs() {
 export function NPCsProvider({ children }: { children: React.ReactNode }) {
   const [NPCs, setNPCs] = useState<NPC[]>(setupNPCs());
 
-  function updateNPCState(npc: NPC, stateId: string, amount: number) {
-    const newNPC = { ...npc };
-    const need = newNPC.needs.find(s => s.id === stateId);
-    if (!need) throw new Error("State not found");
-    const newNeed = { ...need };
-    newNeed.value = Math.min(newNeed.maxValue, Math.max(newNeed.minValue, newNeed.value + amount));
-    newNPC.needs = newNPC.needs.map(s => s.id === stateId ? newNeed : s);
-    setNPCs(prevNPCs => prevNPCs.map(n => n.id === npc.id ? newNPC : n));
+  useEffect(() => {
+    console.log("NPCs after relationship update:", NPCs);
+  }, [NPCs]);
+
+  function updateNPCNeed(npcId: string, stateId: string, amount: number) {
+    function doUpdate(currentNPCs: NPC[], npcId: string, stateId: string, amount: number) {
+      const NPC = currentNPCs.find(n => n.id === npcId);
+      if (!NPC) throw new Error("NPC not found");
+      const newNPC = { ...NPC };
+      const need = newNPC.needs.find(s => s.id === stateId);
+      if (!need) throw new Error("State not found");
+      const newNeed = { ...need };
+      newNeed.value = Math.min(newNeed.maxValue, Math.max(newNeed.minValue, newNeed.value + amount));
+      newNPC.needs = newNPC.needs.map(s => s.id === stateId ? newNeed : s);
+      return newNPC;
+    }
+    setNPCs(prevNPCs => prevNPCs.map(n => n.id === npcId ? doUpdate(prevNPCs, npcId, stateId, amount) : n));
+  }
+
+  function updateNPCRelationship(npcId: string, otherNpcId: string, amount: number) {
+    function doUpdate(currentNPCs: NPC[], npcId: string, otherNpcId: string) {
+      const NPC = currentNPCs.find(n => n.id === npcId);
+      if (!NPC) throw new Error("NPC not found");
+      const newNPC = { ...NPC };
+      const relationship = newNPC.relationships.find(r => r.npcId === otherNpcId);
+      if (!relationship) throw new Error("Relationship not found");
+      const newRelationship = { ...relationship };
+      newRelationship.value += amount;
+      newNPC.relationships = newNPC.relationships.map(r => r.npcId === otherNpcId ? newRelationship : r);
+      return newNPC;
+    }
+    setNPCs(prevNPCs => prevNPCs.map(n => n.id === npcId ? doUpdate(prevNPCs, npcId, otherNpcId) : n));
+    setNPCs(prevNPCs => prevNPCs.map(n => n.id === otherNpcId ? doUpdate(prevNPCs, otherNpcId, npcId) : n));
   }
 
   return (
-    <NPCsContext.Provider value={{ NPCs, setNPCs, updateNPCNeed: updateNPCState }} >
+    <NPCsContext.Provider value={{ NPCs, setNPCs, updateNPCNeed, updateNPCRelationship }} >
       {children}
     </NPCsContext.Provider>
   );
--- src/controllers/activities.tsx
+++ src/controllers/activities.tsx
@@ -16,6 +16,7 @@ interface ActivitiesContextType {
 export interface AssignedActivity {
   activityId: string;
   npcId: string;
+  companyingNPCIds?: string[];
 }
 
 const ActivitiesContext = createContext<ActivitiesContextType | undefined>(undefined);
@@ -29,22 +30,27 @@ export const useActivitiesContext = () => {
 };
 
 export function ActivitiesProvider({ children }: { children: React.ReactNode }) {
-  const { NPCs, updateNPCNeed } = useNPCsContext();
+  const { NPCs, updateNPCNeed, updateNPCRelationship } = useNPCsContext();
   const { updateShipNeed } = useShipContext();
   const { progressDay } = useDayContext();
   const { addLog } = useEventLogs();
+
+  const [activities, setActivities] = useState<Activity[]>(startingActivities);
 
   function doActivity(assignedActivity: AssignedActivity) {
     const activity = activities.find(a => a.id === assignedActivity.activityId);
     const npc = NPCs.find(n => n.id === assignedActivity.npcId);
     if (!activity || !npc) throw new Error("Activity or NPC not found");
     if (activity.actions.length === 0) return;
+    assignedActivity.companyingNPCIds?.forEach(companyingNpcId => {
+      updateNPCRelationship(npc.id, companyingNpcId, 1);
+    });
     const message = []
     message.push(<Text align='left'><Strong>{npc.name}</Strong> is starting activity <Strong>{activity.name}:</Strong></Text>);
     activity.actions.forEach(action => {
       switch (action.type) {
         case actionTypes.NPCNeedChange:
-          updateNPCNeed(npc, action.needId, action.amount);
+          updateNPCNeed(npc.id, action.needId, action.amount);
           break;
         case actionTypes.ShipNeedChange:
           updateShipNeed(action.needId, action.amount);
@@ -56,14 +62,23 @@ export function ActivitiesProvider({ children }: { children: React.ReactNode }) 
       message.push(<Text align='left'>{`${changeEmoji} Changing ${action.needId} by ${action.amount}`}</Text>);
     });
     addLog({ message, type: 'info' });
+    console.log("Completed activity", NPCs);
   }
 
   function doActivities(assignedActivities: AssignedActivity[]) {
-    assignedActivities.forEach(aa => doActivity(aa));
+    const uniqueSocialPairs = new Set<string>();
+    for (const activity of assignedActivities) {
+      activity.companyingNPCIds = assignedActivities.reduce((acc, aa) => {
+        if (aa.activityId === activity.activityId && aa.npcId !== activity.npcId && !uniqueSocialPairs.has([activity.npcId, aa.npcId].sort().join('-'))) {
+          acc.push(aa.npcId);
+          uniqueSocialPairs.add([activity.npcId, aa.npcId].sort().join('-'));
+        }
+        return acc;
+      }, [] as string[]);
+      doActivity(activity);
+    }
     progressDay();
   }
-
-  const [activities, setActivities] = useState<Activity[]>(startingActivities);
 
   return (
     <ActivitiesContext.Provider value={{ activities, setActivities, doActivities }} >
